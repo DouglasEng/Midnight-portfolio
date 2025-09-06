e01dcbf905bcb62da52c8415269b5679166bf323
Douglas <douglasrodriguesgithub@gmail.com>
2025-09-06 12:19:02 -0300

    [ 🏗️ ] Index & Admin - Seção Certificação

--- portfolio-dg/core/admin.py
+++ portfolio-dg/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Projeto
+from .models import Projeto, Certificacao
 
 @admin.register(Projeto)
 class ProjetoAdmin(admin.ModelAdmin):
@@ -18,6 +18,32 @@ class ProjetoAdmin(admin.ModelAdmin):
         }),
         ('Detalhes Técnicos', {
             'fields': ('tecnologias', 'imagem', 'link')
+        }),
+        ('Metadados', {
+            'fields': ('data_criacao',),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(Certificacao)
+class CertificacaoAdmin(admin.ModelAdmin):
+    """
+    Configuração do admin para o modelo Certificacao.
+    Interface otimizada para gerenciar certificações.
+    """
+    list_display = ('titulo', 'organizacao', 'data_obtencao', 'data_criacao')
+    list_filter = ('organizacao', 'data_obtencao', 'data_criacao')
+    search_fields = ('titulo', 'organizacao')
+    readonly_fields = ('data_criacao',)
+    date_hierarchy = 'data_obtencao'
+    
+    fieldsets = (
+        ('Informações da Certificação', {
+            'fields': ('titulo', 'organizacao', 'data_obtencao')
+        }),
+        ('Mídia e Links', {
+            'fields': ('imagem', 'link')
         }),
         ('Metadados', {
             'fields': ('data_criacao',),
