cca4e7fa483a85358ace38e9a6de3defa514fa51
Douglas <douglasrodriguesgithub@gmail.com>
2025-09-06 11:53:04 -0300

    [ 🏗️ ] Index & Admin - Seção de projetos

--- portfolio-dg/core/admin.py
+++ portfolio-dg/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Projeto
 
-# Register your models here.
+@admin.register(Projeto)
+class ProjetoAdmin(admin.ModelAdmin):
+    """
+    Configuração do admin para o modelo Projeto.
+    Interface otimizada para gerenciar projetos do portfólio.
+    """
+    list_display = ('titulo', 'tecnologias', 'data_criacao', 'link')
+    list_filter = ('data_criacao',)
+    search_fields = ('titulo', 'descricao', 'tecnologias')
+    readonly_fields = ('data_criacao',)
+    
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('titulo', 'descricao')
+        }),
+        ('Detalhes Técnicos', {
+            'fields': ('tecnologias', 'imagem', 'link')
+        }),
+        ('Metadados', {
+            'fields': ('data_criacao',),
+            'classes': ('collapse',)
+        }),
+    )
--- portfolio-dg/portfolio/settings.py
+++ portfolio-dg/portfolio/settings.py
@@ -8,6 +8,6 @@ ALLOWED_HOSTS = ['*']  # ou deixar vazio
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_public.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
